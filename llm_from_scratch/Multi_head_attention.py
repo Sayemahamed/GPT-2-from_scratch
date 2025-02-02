eb27946d84a0a8529f3c2a03f73c6724c5227b34
Sayemahamed <sayemahamed183@gmail.com>
2025-02-02 19:23:01 +0600

    implemented MultiHeadAttention

--- llm_from_scratch/Multi_head_attention.py
+++ llm_from_scratch/Multi_head_attention.py
@@ -1,12 +1,48 @@
 from torch import nn
+import torch
+
+
 class MultiHeadAttention(nn.Module):
-    def __init__(self
-                 ,d_in:int,d_out:int,context_length:int,n_heads:int,qkv_bias:bool=False):
-        super().__init__(  )
+    def __init__(
+        self,
+        d_in: int,
+        d_out: int,
+        context_length: int,
+        n_heads: int,
+        qkv_bias: bool = False,
+    ):
+        super().__init__()
         assert d_out % n_heads == 0, "d_out must be divisible by n_heads"
         self.n_heads: int = n_heads
         self.head_dim: int = d_out // n_heads
-        self.query_weight = nn.Linear(in_features=d_in, out_features=d_out, bias=qkv_bias)
+        self.query_weight = nn.Linear(
+            in_features=d_in, out_features=d_out, bias=qkv_bias
+        )
         self.key_weight = nn.Linear(in_features=d_in, out_features=d_out, bias=qkv_bias)
-        self.value_weight = nn.Linear(in_features=d_in, out_features=d_out, bias=qkv_bias)
+        self.value_weight = nn.Linear(
+            in_features=d_in, out_features=d_out, bias=qkv_bias
+        )
         self.out_proj = nn.Linear(in_features=d_out, out_features=d_out)
+        self.mask: torch.Tensor = torch.triu(
+            input=torch.ones(context_length, context_length), diagonal=1
+        )
+        self.register_buffer(name="mask", tensor=self.mask.bool())
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        queries:torch.Tensor=self.query_weight(x)
+        keys:torch.Tensor=self.key_weight(x)
+        values:torch.Tensor=self.value_weight(x)
+
+        b,n,_=x.shape
+
+        queries=queries.view(b,n,self.n_heads,self.head_dim).transpose(1,2)
+        keys=keys.view(b,n,self.n_heads,self.head_dim).transpose(1,2)
+        values=values.view(b,n,self.n_heads,self.head_dim).transpose(1,2)
+
+        scores:torch.Tensor=queries @ keys.transpose(-2,-1) / (self.head_dim**0.5)
+        scores=scores.masked_fill_(self.mask[:n,:n], float("-inf"))
+        attention: torch.Tensor=scores.softmax(dim=-1)
+
+        context: torch.Tensor=attention @ values
+        context=context.transpose(1,2).contiguous().view(b,n,self.n_heads*self.head_dim)
+        return self.out_proj(context)
--- llm_from_scratch/test.py
+++ llm_from_scratch/test.py
@@ -1,7 +1,8 @@
 from data_Processor import create_dataloader
+
 with open("the-verdict.txt") as f:
     text = f.read()
-    loader=create_dataloader(text)
+    loader = create_dataloader(text)
 
 for input, target in loader:
     print("input:", input, "target:", target)
