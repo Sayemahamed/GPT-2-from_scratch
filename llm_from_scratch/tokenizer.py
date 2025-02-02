95e153076be99df39103f9dccb0ffc9b3086524e
Sayemahamed <sayemahamed183@gmail.com>
2025-02-02 11:22:01 +0600

    building Tokenizer class

--- llm_from_scratch/tokenizer.py
+++ llm_from_scratch/tokenizer.py
@@ -2,4 +2,14 @@ import tiktoken
 import os
 
 class Tokenizer:
-    def __init__(self,algorithm:str="gpt2",path:str|None=None):
+    def __init__(self,algorithm:str="gpt2",text_file_path:str|None=None):
+        if text_file_path is None or not os.path.exists(text_file_path):
+            text_file_path = input("Please enter the file path for the text to be tokenized: ")
+            if not os.path.exists(text_file_path):
+                raise ValueError("Please provide a valid file path")
+        with open(text_file_path,"r") as f:
+            text = f.read()
+        self.encoding = tiktoken.get_encoding(algorithm)
+        self.tokens = self.encoding.encode(text,allowed_special={"<|endoftext|>"})
+
+Tokenizer("gpt2","the-verdict.txt")
