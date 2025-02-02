236ec47147d9ca30a244180378ee9fbac7403b67
Sayemahamed <sayemahamed183@gmail.com>
2025-02-02 17:50:36 +0600

    implemented initialization

--- llm_from_scratch/data_Processor.py
+++ llm_from_scratch/data_Processor.py
@@ -1,3 +1,12 @@
 from torch.utils.data import Dataset, DataLoader
 import tiktoken
+import torch 
 class GPTDataset(Dataset):
+    def __init__(self,text:str,tokenizer,max_length:int,stride:int):
+        token:list[int]=tokenizer.encode(text,allowed_special={"<|endoftext|>"})
+        self.data: list[tuple[torch.Tensor, torch.Tensor]]=[
+            (
+                torch.tensor(token[i:i+max_length],dtype=torch.long),
+                torch.tensor(token[i+1:i+1+max_length],dtype=torch.long)
+            )for i in range(0,len(token)-max_length,stride)
+        ]
